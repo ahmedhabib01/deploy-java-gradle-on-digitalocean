ea767ff5fb502442cc82dac20cf6f72500c8a203
Ahmed Habib <72246938+ahmedhabib01@users.noreply.github.com>
2026-01-11 12:24:46 +0100

    Update Main.java

--- app/src/main/java/com/example/app/Main.java
+++ app/src/main/java/com/example/app/Main.java
@@ -6,15 +6,28 @@ import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        int port = 8080;
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
         server.createContext("/", exchange -> {
-            String response = "Hello from Java Gradle app in Codespaces!";
+            String response = "Server is running!";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
             os.close();
         });
+
+        server.createContext("/hello", exchange -> {
+            String response = "{\"message\": \"Hello from Java backend!\"}";
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+
         server.start();
-        System.out.println("Server started on port 8080");
+        System.out.println("Server started on port " + port);
     }
 }
